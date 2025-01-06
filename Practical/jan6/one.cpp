dcfde8813784685c4faa6794273ab1403679e5b2
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-01-06 11:23:05 +0530

    lil wrong

--- Practical/jan6/one.cpp
+++ Practical/jan6/one.cpp
@@ -5,7 +5,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int c) {
+        data = c;
+        next = nullptr;
+    }
+};
+
+void max_LL() {
+    int n;
+    cin >> n;
+
+    Node* head = nullptr;
+    cout << "Enter the elements: ";
+    int tc;
+    for (int i = 0; i < n; i++) {
+        if (head == nullptr) {
+            cin >> tc;
+            head = new Node(tc);
+        } else {
+            cin >> tc;
+            Node* temp = new Node(tc);
+            head->next = temp;
+            head = head->next;
+        }
+    }
+
+    Node* trav = head;
+    while (trav != nullptr) {
+        cout << trav->data << endl;
+        trav = trav->next;
+    }
+}
+
 int main() {
-    cout << "Hello World";
+    max_LL();
     return 0;
 }
