ef03e01e74e34b6c1c3ebca50447751b09fb0e54
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-01-06 11:45:21 +0530

    largest LL

--- Practical/jan6/one.cpp
+++ Practical/jan6/one.cpp
@@ -9,6 +9,10 @@ struct Node {
     int data;
     Node* next;
 
+    Node() {
+        next = nullptr;
+    }
+
     Node(int c) {
         data = c;
         next = nullptr;
@@ -19,27 +23,56 @@ void max_LL() {
     int n;
     cin >> n;
 
-    Node* head = nullptr;
-    cout << "Enter the elements: ";
+    cout << "Enter the elements?:\n";
     int tc;
-    for (int i = 0; i < n; i++) {
-        if (head == nullptr) {
-            cin >> tc;
-            head = new Node(tc);
-        } else {
-            cin >> tc;
-            Node* temp = new Node(tc);
-            head->next = temp;
-            head = head->next;
-        }
+    cin >> tc;
+
+    Node* head = new Node(tc);
+    Node* temp = head;
+
+    for (int i = 0; i < n-1; i++) {
+        cin >> tc;
+        temp->next = new Node(tc);
+        temp = temp->next;
     }
 
     Node* trav = head;
-    while (trav != nullptr) {
-        cout << trav->data << endl;
+    int largest = 0;
+    while (trav!=nullptr) {
+        if (trav->data > largest) {
+            largest = trav->data;
+        }
         trav = trav->next;
     }
+
+    cout << "Largest Element: " << largest << endl;
 }
+
+// void max_LL() {
+//     int n;
+//     cin >> n;
+//
+//     Node* head = nullptr;
+//     cout << "Enter the elements: ";
+//     int tc;
+//     for (int i = 0; i < n; i++) {
+//         if (head == nullptr) {
+//             cin >> tc;
+//             head = new Node(tc);
+//         } else {
+//             cin >> tc;
+//             Node* temp = new Node(tc);
+//             head->next = temp;
+//             head = head->next;
+//         }
+//     }
+//
+//     Node* trav = head;
+//     while (trav != nullptr) {
+//         cout << trav->data << endl;
+//         trav = trav->next;
+//     }
+// }
 
 int main() {
     max_LL();
