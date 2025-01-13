a4cea4efc88e7e7ed3a3dc7dbd995c60ebe12136
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-01-13 10:45:28 +0530

    jan 9 working tree set

--- Practical/jan9/one.cpp
+++ Practical/jan9/one.cpp
@@ -4,3 +4,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int c) {
+        data = c;
+        next = nullptr;
+    }
+};
+
+int main() {
+    Node* head = nullptr;
+    
+
+}
