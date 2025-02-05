297627874daeff0bb5fcb0fec941e79d0cfd45de
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-02-05 23:28:38 +0530

    quicksort

---
+++ Practical/feb04/one.cpp
@@ -0,0 +1,21 @@
+//
+// Created by ariha on 04-02-2025.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> jesus(){
+    int arr[] = {3,6,7,15,19,20};
+    int n = 6;
+
+    int left = arr[0];
+    int right = arr[n-1];
+    //
+    // for (int i = 0; i < n; i++) {
+    //     if () {
+    //
+    //     }
+    // }
+
+
+}
---
+++ Practical/feb05/one.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printA(int arr[]) {
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {10,15,1,2,9,16,11};
+    int n = 7;
+    int i = 0;
+    int j = n-2;
+    int p = arr[n-1];
+
+    while (i < j) {
+        if (arr[i] > p) {
+            
+        }
+    }
+}
