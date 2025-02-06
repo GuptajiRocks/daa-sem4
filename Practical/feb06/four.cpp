db9f333f254a65c784ef54377da25b69a3cd79e2
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-02-06 11:31:35 +0530

    6 feb lol

--- Practical/feb06/four.cpp
+++ Practical/feb06/four.cpp
@@ -12,21 +12,23 @@ int main() {
     vector<int> arr = {65,70,75,80,85,60,55};
     int n = arr.size();
     int p = -1;
+
     
-    // finding pivot element
-    for (int i = 0; i < n; i++) {
-        if (arr[i+1] > arr[i]) {
-            continue;
-        } else {
-            p = i;
-            break;
-        }
-    }
+    
+    // // finding pivot element
+    // for (int i = 0; i < n; i++) {
+    //     if (arr[i+1] > arr[i]) {
+    //         continue;
+    //     } else {
+    //         p = i;
+    //         break;
+    //     }
+    // }
 
-    swap(arr[p], arr[n-1]);
+    // swap(arr[p], arr[n-1]);
 
-    cout << p << endl;
-    printA(arr);
+    // cout << p << endl;
+    // printA(arr);
 
     return 0;
 }
