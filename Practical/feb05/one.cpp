783542476be636c13c2db34d7a81f2b047c8dd81
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-02-06 08:41:39 +0530

    6feb clear

--- Practical/feb05/one.cpp
+++ Practical/feb05/one.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printA(int arr[]) {
-    for (int num : arr) {
-        cout << num << " ";
+void printA(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
     }
     cout << endl;
 }
@@ -15,9 +15,10 @@ int main() {
     int j = n-2;
     int p = arr[n-1];
 
-    while (i < j) {
-        if (arr[i] > p) {
+    // while (i < j) {
+    //     if (arr[i] > p) {
             
-        }
-    }
+    //     }
+    // }
+    printA(arr, n);
 }
