8224cac9ce240cc399a9daa240007287014c1089
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-03-01 20:28:59 +0530

    FCFC c++

---
+++ OSprac/mar01/one.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Process {
+    int PID, at, bt;
+    int ct = 0;
+};
+
+
+int main() {
+    int n = 5;
+    Process arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cout << "Enter PID: "; cin >> arr[i].PID;
+        cout << "Enter Arrival Time: "; cin >> arr[i].at;
+        cout << "Enter Burst Time: "; cin >> arr[i].bt;
+    }
+
+    int gtt = 0;
+    int c = 0;
+
+    while (true) {
+        
+
+
+    }
+
+
+}
