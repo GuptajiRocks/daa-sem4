f987025df54a3e2c2fc677d037d8f584b4c669bf
Arihant Gupta <72906949+GuptajiRocks@users.noreply.github.com>
2025-04-08 09:07:54 +0530

    8apr

---
+++ OSprac/apr08/one.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+    int n;
+    int p;
+    cin >> n >> p;
+
+    int blocksize[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> blocksize[i];
+    }
+
+    int psize[p];
+
+    for (int i = 0; i < p; i++) {
+        cin >> psize[i];
+    }
+    
+    cout << "Process_no:\tProcess_size \tBlock_no:\tBlock_size:\tFragement\n";
+    for (int i = 0; i < p; i++) {
+        
+    }
+
+}
